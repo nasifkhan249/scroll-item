2f9fe8486c341c367e824dcf5f05a1d68539b701
Nasif Mustufa Khan <nasifkhan994@gmail.com>
2024-08-25 18:57:10 +0600

    updated

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 let btn=document.getElementById("btn");
-// console.log(btn);
+let menu=document.getElementById("menu");
+console.log(menu);
+let cross=document.getElementById("cross");
+console.log(cross);
 
 window.onscroll=function(){
     
     scrolling();
 
 }
-
 
 function scrolling(){
     if(document.body.scrollTop > 1 || document.documentElement.scrollTop > 1){
@@ -20,3 +22,27 @@ btn.addEventListener("click",function(){
     document.body.scrollTop=0;
     document.documentElement.scrollTop=0;
 },true);
+
+
+menu.addEventListener("click",()=>{
+    document.getElementById("menu").style.display="none";
+    document.getElementById("cross").style.display="block";
+
+    document.getElementById("nav-item").style.display="block";
+    document.getElementById("nav-bar").style.height="100vh";
+    document.getElementById("nav-bar").style.display="flex";
+    document.getElementById("nav-bar").style.flexDirection="column";
+    document.getElementById("nav-bar").style.justifyContent="center";
+    document.getElementById("nav-bar").style.alignItems="center";
+    document.getElementById("logo").style.paddingRight="40px";
+    document.getElementById("logo").style.paddingBottom="20px";
+    document.getElementById("nav-item").style.gap="40px";
+
+});
+
+cross.addEventListener("click",()=>{
+    document.getElementById("cross").style.display="none";
+    document.getElementById("menu").style.display="block";
+    document.getElementById("nav-item").style.display="none";
+    window.location.reload();
+})
